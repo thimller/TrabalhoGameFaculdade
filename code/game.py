a5d0a660a9e209d218a257c7caf912bd8e372640
Thi <thi.garbuio@hotmail.com>
2025-03-17 21:47:06 -0300

    Adição das imagens Criação do retangulo e import imagem do menu

--- code/game.py
+++ code/game.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame
+
+from code.const import WIN_WIDTH, WIN_HEIGHT
 from code.menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self, ):
 
--- code/menu.py
+++ code/menu.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
+        # add imagem
+        self.surf = pygame.image.load("./asset/MenuPNG.png")
+        # cria um retangulo
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
+        #desenhando a imagem no retangulo
+        self.window.blit(source=self.surf, dest=self.rect)
+        #mostrando a imagem na tela
+        pygame.display.flip()
         pass
