3b321a313e52a6a34088a0799c708c04c80b71be
Thi <thi.garbuio@hotmail.com>
2025-03-17 22:57:27 -0300

    Adição da primeira parte do Titulo Obs: poderá ser alterado

--- code/game.py
+++ code/game.py
@@ -12,17 +12,8 @@ class Game:
         self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
-        # carrega a musica
-        pygame.mixer_music.load("./asset/Musicas/Menu.musica.mp3")
-        # faz a musica tocar
-        pygame.mixer_music.play(-1)
 
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-
-            #for event in pygame.event.get():
-             #   if event.type == pygame.QUIT:
-              #      pygame.QUIT()
-               #     quit()
--- code/menu.py
+++ code/menu.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.const import WIN_WIDTH
 
 
 class Menu:
@@ -12,8 +16,31 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        #desenhando a imagem no retangulo
-        self.window.blit(source=self.surf, dest=self.rect)
-        #mostrando a imagem na tela
-        pygame.display.flip()
-        pass
+
+        # carrega a musica
+        pygame.mixer_music.load("./asset/Musicas/Menu.musica.mp3")
+        # faz a musica tocar
+        pygame.mixer_music.play(-1)
+
+        while True:
+            # desenhando a imagem no retangulo
+            self.window.blit(source=self.surf, dest=self.rect)
+
+            self.menu_text(50,"Castelo",(200,0,0),((WIN_WIDTH/2), 70))
+            # mostrando a imagem na tela
+            pygame.display.flip()
+
+            #checa o evento para poder fechar o prog no 'x'
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.QUIT()
+                    quit()
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+
+        text_surf: Surface = text_font.render(text,True,text_color).convert_alpha()
+
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+
+        self.window.blit(source=text_surf, dest=text_rect)
