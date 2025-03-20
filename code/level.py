119caa19e0a09c2319d2cbfa6dcb2a159763d742
thimller <130771408+thimller@users.noreply.github.com>
2025-03-20 17:15:44 -0300

    Update level.py
    Inicialização do level1

--- code/level.py
+++ code/level.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 class Level:
-    def __init__(self):
-        self.window = None
-        self.name = None
-        self.entity_list = None
+    def __init__(self, window, name, game_mode):
+        self.window = window
+        self.name = name
+        self.game_mode = game_mode
+        self.entity_list: list[Entity] = []
 
     def run(self, ):
         pass
