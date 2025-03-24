b7670915df38fe9aba47a5edd42fabdafd5caac1
thimller <130771408+thimller@users.noreply.github.com>
2025-03-24 11:43:38 -0300

    Update player.py
    Adicionado as teclas de movimentação para esquerda e direita

--- code/player.py
+++ code/player.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.key
 
-class Player:
-    def __init__(self):
-        pass
+from code.entity import Entity
+from code.const import ENTITY_SPEED, WIN_WIDTH
+
+
+class Player(Entity):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
 
     def move(self, ):
+        pressed_key = pygame.key.get_pressed()
+        if pressed_key[pygame.K_LEFT] and self.rect.left > 0:
+            self.rect.centerx -= ENTITY_SPEED[self.name]
+        if pressed_key[pygame.K_RIGHT] and self.rect.right < WIN_WIDTH:
+            self.rect.centerx += ENTITY_SPEED[self.name]
         pass
